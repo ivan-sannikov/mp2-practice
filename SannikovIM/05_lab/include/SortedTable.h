17d1bf336590f2a3682f0a750eaf7f15481c07de
ivan-sannikov <sanvan0094@gmail.com>
2025-05-12 16:00:22 +0300

    nc v1.5

--- SannikovIM/05_lab/include/SortedTable.h
+++ SannikovIM/05_lab/include/SortedTable.h
@@ -5,11 +5,14 @@
 template <typename TKey, typename TData>
 class SortedTable : public ScanTable<TKey, TData> {
 private:
+	TabRecord<TKey, TData>* binarySearch(TKey key);
 	void BubleSort();
 	void QuickSort();
-
+	void SelectionSort();
+	void InsertSort();
 public:
 	SortedTable(int maxSize);
+	SortedTable(ScanTable<TKey, TData>& table);
 	void Insert(TKey key, TData data);
 	void Remove(TKey key);
 	TabRecord<TKey, TData>* Find(TKey key);
@@ -19,40 +22,95 @@ public:
 
 template <typename TKey, typename TData>
 void SortedTable<TKey, TData>::BubleSort() {
-	for(int i = 0; i<this->maxSize;i++){
-		for(int j = 0; j<this->maxsSize - i - 1; j++){
-			try{
-
-			}catch(const exception& e);
+	for(int i = 0; i<this->count;i++){
+		for(int j = i; j< this->count; j++){
+			if (this->recs[i]->key > this->recs[j]->key) {
+				TabRecord<TKey, TData>* tmp = recs[i];
+				this->recs[i] = this->recs[j];
+				this->recs[j] = tmp;
+			}
 		}
+	}
+
+}
+template <typename TKey, typename TData>
+TabRecord<TKey, TData>*  SortedTable<TKey, TData>::binarySearch(TKey key) {
+	int low = 0;
+	int high = this->count-1;
+	while (low <= high) {
+		int mid = ((high - low) / 2) + low;
+		if (this->recs[mid]->key == key) {
+			return this->recs[mid];
+		}
+		if (this->recs[mid]->key > key)
+			high = mid - 1;
+		if (this->recs[mid]->key < key)
+			low = mid + 1;
+
+	}
+	return nullptr;
+}
+
+template <typename TKey, typename TData>
+void SortedTable<TKey, TData>::SelectionSort() {
+	for (int i = 0; i < this->count - 1; i++) {
+		int min_id = i;
+		for (int j = i + 1; j < this->count; j++) {
+			if (this->recs[j]->key < this->recs[min_id]->key)
+				min_id = j;
+		}
+		TabRecord<TKey, TData>* tmp = recs[i];
+		this->recs[i] = this->recs[min_id];
+		this->recs[min_id] = tmp;
+	}
+}
+template <typename TKey, typename TData>
+void SortedTable<TKey, TData>::InsertSort() {
+	for (int i = 1; i < this->count; i++) {
+		TabRecord<TKey, TData>* key = this->recs[i];
+		int j = i - 1;
+		while (j >= 0 && this->recs[j]->key > key->key) {
+			this->recs[j + 1] = this->recs[j];
+			j = j - 1;
+		}
+		this->recs[j + 1] = key;
 	}
 }
 
-template <typename TKey, typename TData>
-void SortedTable<TKey, TData>::Remove(TKey key) {
-	if (key > this->maxSize) throw "error";
-	recs[key] = nullptr;
-}
-
-
-
 
 template <typename TKey, typename TData>
-
 SortedTable<TKey, TData>::SortedTable(int maxSize) : ScanTable<TKey, TData>(maxSize) {
 	static_assert(std::is_arithmetic<TKey>::value, "error");
 }
 
 template <typename TKey, typename TData>
+SortedTable<TKey, TData>::SortedTable(ScanTable<TKey, TData>& table) : ScanTable<TKey, TData>(table) {
+	InsertSort();
+}
+
+template <typename TKey, typename TData>
 void SortedTable<TKey, TData>::Insert(TKey key, TData data) {
-	if (key > this->maxSize) throw "key is more then table size.";
-	this->currPos++;
-	this->recs[count++] = new TabRecord<TKey, TData>(key, data);
+	TabRecord<TKey, TData>* tmp = this->binarySearch(key);
+	int i = 0;
+
+	while(i<this->count+1)
 }
 template <typename TKey, typename TData>
 TabRecord<TKey, TData>* SortedTable<TKey, TData>::Find(TKey key) {
-	if (key > this->maxSize) throw "error";
-	return recs[key];
+	int low = 0;
+	int high = this->count - 1;
+	while (low <= high) {
+		int mid = ((high - low) / 2) + low;
+		if (this->recs[mid]->key == key) {
+			return this->recs[mid];
+		}
+		if (this->recs[mid]->key > key)
+			high = mid - 1;
+		if (this->recs[mid]->key < key)
+			low = mid + 1;
+
+	}
+	return nullptr;
 }
 template <typename TKey, typename TData>
 void SortedTable<TKey, TData>::Remove(TKey key) {
--- SannikovIM/05_lab/test/test_sortedtable.cpp
+++ SannikovIM/05_lab/test/test_sortedtable.cpp
@@ -6,3 +6,23 @@ TEST(SortedTable, can_make_scantable) {
     SortedTable<int, int> a(10);
     ASSERT_NO_THROW(a);
 }
+TEST(SortedTable, can_make_sortedtable_from_scantable) {
+    ScanTable<int, int> a(3);
+    ScanTable<int, int> b(3);
+    a.Insert(1, 1);
+    a.Insert(2, 1);
+    a.Insert(3, 1);
+    b.Insert(3, 1);
+    b.Insert(1, 1);
+    b.Insert(2, 1);
+   
+    SortedTable<int, int> c(b);
+    EXPECT_EQ(c.GetCurrent()->key, a.GetCurrent()->key);
+}
+TEST(SortedTable, can_find_in_sortedtable) {
+    SortedTable<int, int> a(10);
+    a.Insert(1, 1);
+    a.Insert(2, 1);
+    a.Insert(3, 8);
+    EXPECT_EQ(a.Find(3)->pData, 8);
+}
